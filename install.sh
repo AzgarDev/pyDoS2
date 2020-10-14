d1d6e2341e78f67b9a12ad8198a0ee51f20c51d4
AzgarDev <71645505+AzgarDev@users.noreply.github.com>
2020-10-14 09:47:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ apt-get install python3-pip
 apt-get install python-pip
 pip install colorama
 pip install sockets
+pip install emoji
 pip3 install colorama
 pip3 install sockets
+pip3 install emoji
 echo "DONE MY FRIEND"
 sleep 2
